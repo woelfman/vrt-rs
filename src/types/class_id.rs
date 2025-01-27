c08ab64a4d07bdaad07c59697e2ee0a8235b06ef
Matt Woelfel <matt@woelfware.com>
2025-01-27 15:07:46 -0600

    Remove nom-derive
    The last release is a couple of years old with some outstanding PRs, so
    I'm not sure it's maintained anymore. It has a hard dependency on nom 7
    which prevents us from moving to nom 8.

--- src/types/class_id.rs
+++ src/types/class_id.rs
@@ -1,5 +1,10 @@
+use nom::{
+    number::streaming::{be_u16, be_u32},
+    IResult,
+};
+
 /// Class Identifier
-#[derive(Clone, Copy, Debug, Eq, PartialEq, nom_derive::NomBE)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct ClassId {
     /// Organizationally Unique Identifier assigned by IEEE, VITA, the VRT Profile author, or a reserved OUI.
     pub oui: u32,
@@ -7,4 +12,22 @@ pub struct ClassId {
     pub packet_class_code: u16,
     /// Information Class Code
     pub information_class_code: u16,
+}
+
+impl ClassId {
+    /// Parse the Class ID
+    pub fn parse(i: &[u8]) -> IResult<&[u8], ClassId> {
+        let (i, oui) = be_u32(i)?;
+        let (i, packet_class_code) = be_u16(i)?;
+        let (i, information_class_code) = be_u16(i)?;
+
+        Ok((
+            i,
+            ClassId {
+                oui,
+                packet_class_code,
+                information_class_code,
+            },
+        ))
+    }
 }
